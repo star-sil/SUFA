ff37b10c034a66c5e8cb2305639424410e18f607
star-sil <kimse9811@naver.com>
2022-12-04 16:54:13 +0900

    feat: setup sufa project

---
+++ src/test/java/com/example/sufa/SufaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sufa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SufaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
